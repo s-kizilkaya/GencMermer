0a0b78f24809d454598a16af1a8791cb210bea77
2NMaster <2NMaster@DESKTOP-KL01N7V>
2025-07-04 12:50:28 +0300

    iconColors

--- pages-sections/IletisimPage-Sections/IletisimBilgileriSection.js
+++ pages-sections/IletisimPage-Sections/IletisimBilgileriSection.js
@@ -8,10 +8,7 @@ import GridContainer from "/components/Grid/GridContainer.js";
 import GridItem from "/components/Grid/GridItem.js";
 import InfoArea from "/components/InfoArea/InfoArea.js";
 
-import {
-  IconPhone,
-  IconMapPin,
-} from "@tabler/icons-react";
+import { IconPhone, IconMapPin } from "@tabler/icons-react";
 
 import styles from "/styles/jss/nextjs-material-kit/pages/AnasayfaPageSections/productStyle.js";
 
@@ -26,30 +23,27 @@ export default function IletisimBilgileriSection() {
         <GridItem xs={12} sm={12} md={12}>
           <h2 className={classes.title}>İletişim Bilgilerimiz</h2>
           <h5 className={classes.description}>
-            Aşağıdaki iletişim bilgilerinden bize ulaşabilir, sorularınızı sorabilir ve projeleriniz hakkında bilgi alabilirsiniz.
-            En kısa sürede size geri dönüş yapacağız.
+            Aşağıdaki iletişim bilgilerinden bize ulaşabilir, sorularınızı
+            sorabilir ve projeleriniz hakkında bilgi alabilirsiniz. En kısa
+            sürede size geri dönüş yapacağız.
           </h5>
         </GridItem>
       </GridContainer>
       <GridContainer>
         <GridItem xs={12} sm={12} md={6}>
           <InfoArea
-          title="Adres"
-          description="Uluönder Mahallesi, Baksan Sanayi Sitesi 81 / 4, 26200 Tepebaşı / Eskişehir, Türkiye"
-          iconColor="primary"
-          vertical
-          icon={<IconMapPin />}
+            title="Adres"
+            description="Uluönder Mahallesi, Baksan Sanayi Sitesi 81 / 4, 26200 Tepebaşı / Eskişehir, Türkiye"
+            vertical
+            icon={<IconMapPin />}
           />
         </GridItem>
         <GridItem xs={12} sm={12} md={6}>
           <InfoArea
-          title="Telefon"
-          description="+90 (532) 592 81 00"
-          iconColor="primary"
-          vertical
-          icon={      
-            <IconPhone />
-          }
+            title="Telefon"
+            description="+90 (532) 592 81 00"
+            vertical
+            icon={<IconPhone />}
           />
         </GridItem>
       </GridContainer>
