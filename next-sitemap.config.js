6a27456329c08ea7a211cddd3880e79e0a0ffb6d
2NMaster <2NMaster@DESKTOP-KL01N7V>
2025-07-04 15:06:00 +0300

    site map

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+// next-sitemap.config.js
+module.exports = {
+  siteUrl: "https://www.orneksite.com", // kendi domainini buraya yaz
+  generateRobotsTxt: true, // robots.txt de otomatik oluşturulsun
+};
